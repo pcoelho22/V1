87b65c26196035b814e845b95bb075c88073b59b
Garfield9110 <prfabri@yahoo.fr>
2016-06-01 15:01:29 +0200

    changement nom fichier css

---
+++ inc/view_add_edit.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php require ('inc/header.php') ?>
+	<link rel="stylesheet" type="text/css" href="css/styleAddEdit.css">
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
