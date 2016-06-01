788c3b719c86f6f7409072765ca3dc609fed2cbd
raven095 <raven90_94@hotmail.com>
2016-06-01 14:55:14 +0200

    Changements nav

--- inc/nav.php
+++ inc/nav.php
@@ -1,25 +1,17 @@
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
 
-	<header>
-		<nav>
-			<ul>
-				<li><a href="index.php">Accueil</a></li>
-				<li><a href="edit_category.php">Catégories</a></li>
-				<li><a href="add_edit.php">Ajouter un film</a></li>
-				<li style="float:right">
-					<form action="" method="post">
-						<input type="text" name="search" value="" placeholder="Search..." />
-						<li style="float:right"><input type="submit" value="REchercher" /></li>
-					</form>
-				</li>
-			</ul>
-		</nav> 
-	</header>
-</body>
-</html>
+
+<div id="nav">	
+	<nav>
+		<ul>
+			<li><a href="index.php">Accueil</a></li>
+			<li><a href="edit_category.php">Catégories</a></li>
+			<li><a href="add_edit.php">Ajouter un film</a></li>
+			
+		</ul>
+	</nav> 
+	<form action="" method="post">
+		<input type="text" name="search" value="" placeholder="Search..." />
+		<li style="float:right"><input type="submit" value="REchercher" /></li>
+	</form>
+</div>
