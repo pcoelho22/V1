256968e7c59b69e69dcd78e08d39ec4dd26c35c8
triggerbite <craimundo75@gmail.com>
2016-06-02 16:56:40 +0200

    add category roll menu

--- edit_category.php
+++ edit_category.php
@@ -1,14 +1,41 @@
 <?php
 // Je crée PDO
-require 'list.php';
 require 'inc/db.php';
 
+// Mettre ici le code pour la recherche
+if (!empty($_GET['cat_id'])) {
+	$cat_id = $_GET['cat_id'];
 
+	$cat_name = array();//
 
+	$sql = '
+		SELECT cat_name
+				FROM category
+		        WHERE cat_id = :cat_id
+		';
+
+	$pdoStatement = $pdo->prepare($sql);
+
+	// je donne la valeur au paramètre de requete
+	$pdoStatement->bindValue(':cat_id',$cat_id);
+
+// Si erreur
+if ($pdoStatement->execute() === false) {
+	print_r($pdo->errorInfo());
+	}
+else if ($pdoStatement->rowCount() > 0) {
+	$cat_info = $pdoStatement->fetch();
+	}
+}
+
+$sql = "SELECT cat_name, cat_id FROM category";
+$pdoStatement = $pdo->prepare($sql);
+// Si erreur
+if ($pdoStatement->execute()) {
+	$cat_list = $pdoStatement->fetchAll();
+}
 
 
 
 // J'affiche ma page
-require 'inc/nav.php';
 require 'inc/view_edit_category.php';
-require 'inc/view_list.php';
--- inc/view_edit_category.php
+++ inc/view_edit_category.php
@@ -1,23 +1,14 @@
 <h3>Liste des catégories</h3>
-<form action="search.php" method="get">
-	<input type="text" name="search">
-	<input type="submit" value="Rechercher">
-</form>
-
-<form method="get">
-	<input type="hidden" name="cat_name" value="<?=$mcapp_sql1?>">
-	<select name="cat_name">
-		<option value="cat_id"></option>
+<form action="edit_category.php" method="get">
+	<select name="cat_id">
+		<?php foreach ($cat_list as $key => $value): ?>
+			<option value="<?= $value['cat_id'] ?>"><?= $value['cat_name'] ?></option>
+		<?php endforeach; ?>
 	</select>
 	<input type="submit" value="OK">
 </form>
-<h3>Liste des films</h3>
-<table>
-	<thead>
-		<tr>
-			<td>Affiche</td>
-			<td>Titre du film</td>
+<form action="edit_category.php" method="get">
+	<input type="text" name="search">
+	<input type="submit" value="Ajouter nouvelle catégorie">
+</form>
 
-		</tr>
-	</thead>
-</table>
