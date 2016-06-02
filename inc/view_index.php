e54cfcda90d21892706c21d78dac70c871fda799
pcoelho22 <webdev@psgroup.lu>
2016-06-02 15:29:58 +0200

    modif
    Adelino

---
+++ inc/view_index.php
@@ -0,0 +1,25 @@
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<form action="" method="post">
+		<input type="text" name="search" value="" placeholder="Search..." />
+		<input type="submit" value="rechercherHome" />
+</form>
+
+<ul>
+	<li><a href=""></a>name cat</li>
+	<li><a href=""></a>name cat</li>
+	<li><a href=""></a>name cat</li>
+	<li><a href=""></a>name cat</li>
+</ul>
+
+<ul>
+	<li><img src=""><br><p>title mov</p></li>
+	<li><img src=""><br><p>title mov</p></li>
+	<li><img src=""><br><p>title mov</p></li>
+	<li><img src=""><br><p>title mov</p></li>
+</ul>
